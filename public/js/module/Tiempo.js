14f96c71656998ada84dc2b6a01c2a47afcaa249
Leonard Andres Amaya Daza <sw4@bromteck.com>
2023-01-04 11:51:57 -0300

    verificacion del online

---
+++ public/js/module/Tiempo.js
@@ -0,0 +1,15 @@
+// function Tiempo(){
+//     if (navigator.onLine == true) {
+//         console.log('online')
+//     }else{alert('offline')}
+// }
+
+// setInterval(function(){window.navigator.onLine},1000)
+
+// program to display a text using setInterval method
+
+function saludar() {
+    console.log("Hola Leonardo")
+}
+
+setInterval(saludar, 1000)
