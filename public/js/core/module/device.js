14b70b300ab011c73c2b6465757af631edc62c53
Leonard Andres Amaya Daza <sw4@bromteck.com>
2023-02-09 11:45:36 -0300

    Test language

--- public/js/core/module/device.js
+++ public/js/core/module/device.js
@@ -136,7 +136,7 @@ Device = (function(Events) {
 		 * @returns {String} Should be in ISO 639-1 (en-gb, cs-cz,...)
 		 */
 		getLanguage: function() {
-			var language = null;
+			var language = "EN";
 
 			try {
 				language = String(window.navigator.language).toLowerCase();
