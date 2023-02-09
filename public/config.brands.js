95573dc80b5c9ec849c119bf8164ee36ee01bd5f
Leonard Andres Amaya Daza <sw4@bromteck.com>
2023-02-09 11:13:21 -0300

    Prueba Test

--- public/config.brands.js
+++ public/config.brands.js
@@ -108,6 +108,16 @@ var CONFIG_BRANDS = [
     epgLineColorTime: "#2CE308",
     developedBy: "Network Broadcast",
   },
+  {
+    brand: "test",
+    appName: "test",
+    drm: "https://pmdw-1.in.tv.br/",
+    token: "CQSepFFsoFNgyLNDYOpz",
+    logoPositionHome: "top",
+    showTime: true,
+    epgLineColorTime: "#2CE308",
+    developedBy: "Network Broadcast",
+  },
 ];
 
 function _getBrand(brand) {
